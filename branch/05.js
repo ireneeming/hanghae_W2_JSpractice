4ca796a4e599194d976e28a625f234ca20e289da
ireneeming <63698668+ireneeming@users.noreply.github.com>
2021-11-08 13:40:38 +0900

    commit

---
+++ branch/05.js
@@ -0,0 +1,16 @@
+
+/*
+문자열 s를 숫자로 변환한 결과를 반환하는 함수, solution을 완성하세요.
+*/
+
+//1번 방법
+function solution(s) {
+    var answer = parseInt(s);
+    return answer;
+}
+
+//2번방법
+
+function solution(s) {
+   return parseInt(s);
+}
