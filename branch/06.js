8dd8c22885b80cd322fed29abf202dd02956a8d9
ireneeming <63698668+ireneeming@users.noreply.github.com>
2021-11-08 19:35:37 +0900

    코드수정

--- branch/06.js
+++ branch/06.js
@@ -2,6 +2,10 @@
 06. 
 0부터 9까지의 숫자 중 일부가 들어있는 배열 numbers가 매개변수로 주어집니다. numbers에서 찾을 수 없는 0부터 9까지의 숫자를 모두 찾아 더한 수를 return 하도록 solution 함수를 완성해주세요.
 
+references: 
+https://velog.io/@mygomi/TIL-69
+https://hianna.tistory.com/408
+https://www.delftstack.com/ko/howto/javascript/javascript-sum-of-array/
 */
 
 
